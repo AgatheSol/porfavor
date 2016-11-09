c09295498a1cd789d3eab722fe1bd4371e9f9246
mblancosoto <mblancosoto@victoria24.danielcastelao.org>
2016-11-09 08:56:13 +0100

    comentario 1

---
+++ src/boletin/pkg9_2/SumaProducto.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg9_2;
+
+/**
+ *
+ * @author mblancosoto
+ */
+public class SumaProducto {
+    
+    
+    public void sumasBoletin(){
+        
+       
+        
+       
+        for(int i=10;i<90;i++){
+            for(int a=10;a<90;a++){
+                System.out.println(" La suma es "+(i+a)+" y "+"la multiplicación es "+(i*a));
+            }
+                
+        }
+         
+        
+            
+            
+        
+       
+        
+            }
+}
